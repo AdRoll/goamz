112bb930380d1593881065b3725b8e4974f952d2
Dave Cheney <david.cheney@canonical.com>
2012-07-06 13:40:25 +1000

    gofmt

--- s3/s3.go
+++ s3/s3.go
@@ -55,7 +55,7 @@ func New(auth aws.Auth, region aws.Region) *S3 {
 // Bucket returns a Bucket with the given name.
 func (s3 *S3) Bucket(name string) *Bucket {
 	if s3.Region.S3BucketEndpoint != "" || s3.Region.S3LowercaseBucket {
-    		name = strings.ToLower(name)
+		name = strings.ToLower(name)
 	}
 	return &Bucket{s3, strings.ToLower(name)}
 }
