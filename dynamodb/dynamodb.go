56d2d06d7f2bc227ef786f0cd81df5e3802f11ce
Matthew Moore <matt@crowdmob.com>
2013-04-01 15:40:21 -0700

    First steps torwards the first dynamodb command

---
+++ dynamodb/dynamodb.go
@@ -0,0 +1,328 @@
+//
+// goamz:dynamodb - Go packages to interact with the Amazon Web Services' DynamoDB.
+//
+//   https://wiki.ubuntu.com/goamz
+//   https://github.com/crowdmob/goamz
+//
+// Copyright (c) 2013 CrowdMob Inc.
+//
+// Written by Matthew Moore <matt@crowdmob.com>
+//
+
+package dynamodb
+
+import (
+  "io"
+	"io/ioutil"
+  "log"
+  "encoding/json"
+  "net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+  "bytes"
+  "strings"
+  "strconv"
+  "fmt"
+  "time"
+  
+
+	"launchpad.net/goamz/aws"
+)
+
+const debug = true
+
+// The DynamoDB type encapsulates operations with an AWS region.
+type DynamoDB struct {
+	aws.Auth
+	aws.Region
+	private byte // Reserve the right of using private data.
+}
+
+// The Table type encapsulates operations with an DynamoDB table.
+type Table struct {
+	*DynamoDB
+	TableName string
+  KeySchema KeySchemaDescriptor
+  ProvisionedThroughput Throughput 
+}
+
+type AttributeNameAndType struct {
+  AttributeName string
+  AttributeType string
+}
+
+type Throughput struct {
+  ReadCapacityUnits int
+  WriteCapacityUnits int
+}
+
+type KeySchemaDescriptor struct {
+	HashKeyElement AttributeNameAndType
+  RangeKeyElement AttributeNameAndType
+}
+
+// New creates a new DynamoDB.
+func New(auth aws.Auth, region aws.Region) *DynamoDB {
+	return &DynamoDB{auth, region, 0}
+}
+
+
+// CreateTable creates a new table.
+//
+// See http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/API_CreateTable.html for details.
+func (t *Table) CreateTable() error {
+	headers := map[string][]string{
+		"x-amz-target": { "DynamoDB_20111205.CreateTable" },
+    "content-type": { "application/x-amz-json-1" },
+  }
+	
+	payld, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+  
+  req := &request{
+		method:  "PUT",
+		path:    "/",
+		headers: headers,
+    payload: bytes.NewReader(payld),
+	}
+  
+	return t.DynamoDB.query(req, nil)
+}
+
+// -----------------------------------------------------
+//   Generic Request Helpers
+// -----------------------------------------------------
+
+type request struct {
+	method   string
+	table    string
+	path     string
+	signpath string
+	params   url.Values
+	headers  http.Header
+	baseurl  string
+	payload  io.Reader
+	prepared bool
+}
+
+
+func (req *request) url() (*url.URL, error) {
+	u, err := url.Parse(req.baseurl)
+	if err != nil {
+		return nil, fmt.Errorf("bad dynamodb endpoint URL %q: %v", req.baseurl, err)
+	}
+	u.RawQuery = req.params.Encode()
+	u.Path = req.path
+	return u, nil
+}
+
+// query prepares and runs the req request.
+// If resp is not nil, the JSON data contained in the response
+// body will be unmarshalled on it.
+func (dynamoDb *DynamoDB) query(req *request, resp interface{}) error {
+	err := dynamoDb.prepare(req)
+	if err == nil {
+		_, err = dynamoDb.run(req, resp)
+	}
+	return err
+}
+
+// prepare sets up req to be delivered to DynamoDB.
+func (dynamoDb *DynamoDB) prepare(req *request) error {
+	if !req.prepared {
+		req.prepared = true
+		if req.method == "" {
+			req.method = "GET"
+		}
+		// Copy so they can be mutated without affecting on retries.
+		params := make(url.Values)
+		headers := make(http.Header)
+		for k, v := range req.params {
+			params[k] = v
+		}
+		for k, v := range req.headers {
+			headers[k] = v
+		}
+		req.params = params
+		req.headers = headers
+		if !strings.HasPrefix(req.path, "/") {
+			req.path = "/" + req.path
+		}
+		req.signpath = req.path
+		if req.table != "" {
+			req.baseurl = strings.Replace(dynamoDb.Region.EC2Endpoint, "ec2", "dynamodb", -1)
+			if req.baseurl == "" {
+				// Use the path method to address the table.
+				req.baseurl = strings.Replace(dynamoDb.Region.EC2Endpoint, "ec2", "dynamodb", -1)
+				req.path = "/" + req.table + req.path
+			} else {
+				// Just in case, prevent injection.
+				if strings.IndexAny(req.table, "/:@") >= 0 {
+					return fmt.Errorf("bad DynamoDB table: %q", req.table)
+				}
+				req.baseurl = strings.Replace(req.baseurl, "${table}", req.table, -1)
+			}
+			req.signpath = "/" + req.table + req.signpath
+		}
+	}
+
+	// Always sign again as it's not clear how far the
+	// server has handled a previous attempt.
+	u, err := url.Parse(req.baseurl)
+	if err != nil {
+		return fmt.Errorf("bad DynamoDB endpoint URL %q: %v", req.baseurl, err)
+	}
+	req.headers["Host"] = []string{u.Host}
+	req.headers["Date"] = []string{time.Now().In(time.UTC).Format(time.RFC1123)}
+
+	err = SignV4("dynamodb", dynamoDb.Region.Name, &dynamoDb.Auth, req)
+  // sign(dynamoDb.Auth, req.method, req.signpath, req.params, req.headers) // maybe change back to this format eventually
+	return nil
+}
+
+// run sends req and returns the http response from the server.
+// If resp is not nil, the XML data contained in the response
+// body will be unmarshalled on it.
+func (dynamoDb *DynamoDB) run(req *request, resp interface{}) (*http.Response, error) {
+	if debug {
+		log.Printf("Running DynamoDB request: %#v", req)
+	}
+
+	u, err := req.url()
+	if err != nil {
+		return nil, err
+	}
+
+	hreq := http.Request{
+		URL:        u,
+		Method:     req.method,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Close:      true,
+		Header:     req.headers,
+	}
+
+	if v, ok := req.headers["Content-Length"]; ok {
+		hreq.ContentLength, _ = strconv.ParseInt(v[0], 10, 64)
+		delete(req.headers, "Content-Length")
+	}
+	if req.payload != nil {
+		hreq.Body = ioutil.NopCloser(req.payload)
+	}
+
+	hresp, err := http.DefaultClient.Do(&hreq)
+	if err != nil {
+		return nil, err
+	}
+	if debug {
+		dump, _ := httputil.DumpResponse(hresp, true)
+		log.Printf("} -> %s\n", dump)
+	}
+	if hresp.StatusCode != 200 && hresp.StatusCode != 204 {
+		return nil, buildError(hresp)
+	}
+	if resp != nil {
+  	json.NewDecoder(hresp.Body).Decode(&resp)
+		hresp.Body.Close()
+	}
+	return hresp, err
+}
+
+
+
+// Error represents an error in an operation with S3.
+type Error struct {
+	StatusCode int    // HTTP status code (200, 403, ...)
+	Code       string // EC2 error code ("UnsupportedOperation", ...)
+	Message    string // The human-oriented error message
+	BucketName string
+	RequestId  string
+	HostId     string
+}
+
+func (e *Error) Error() string {
+	return e.Message
+}
+
+func buildError(r *http.Response) error {
+	if debug {
+		log.Printf("got error (status code %v)", r.StatusCode)
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("\tread error: %v", err)
+		} else {
+			log.Printf("\tdata:\n%s\n\n", data)
+		}
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	}
+
+	err := Error{}
+	// TODO return error if Unmarshal fails?
+	json.NewDecoder(r.Body).Decode(&err)
+	r.Body.Close()
+	err.StatusCode = r.StatusCode
+	if err.Message == "" {
+		err.Message = r.Status
+	}
+	if debug {
+		log.Printf("err: %#v\n", err)
+	}
+	return &err
+}
+
+func shouldRetry(err error) bool {
+	if err == nil {
+		return false
+	}
+	switch err {
+	case io.ErrUnexpectedEOF, io.EOF:
+		return true
+	}
+	switch e := err.(type) {
+	case *net.DNSError:
+		return true
+	case *net.OpError:
+		switch e.Op {
+		case "read", "write":
+			return true
+		}
+	case *Error:
+		switch e.Code {
+		case "InternalError", "NoSuchUpload", "NoSuchBucket":
+			return true
+		}
+	}
+	return false
+}
+
+func hasCode(err error, code string) bool {
+	dynamoerr, ok := err.(*Error)
+	return ok && dynamoerr.Code == code
+}
+
+
+/*
+ TODO The supported functions in the dynamodb operations list at
+ http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/operationlist.html 
+ are as follows. It would be great to support all of these at some point as well.
+
+ Implemented functions have a √ before them.
+
+  BatchGetItem
+  BatchWriteItem
+√ CreateTable
+  DeleteItem
+  DeleteTable
+  DescribeTable
+  GetItem
+  ListTables
+  PutItem
+  Query
+  Scan
+  UpdateItem
+  UpdateTable
+
+*/
---
+++ dynamodb/sign.go
@@ -0,0 +1,208 @@
+package dynamodb
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"goamz/aws"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+)
+
+const iSO8601BasicFormat = "20060102T150405Z"
+const iSO8601BasicFormatShort = "20060102"
+
+var (
+	ErrNoDate = errors.New("Date header not supplied")
+)
+
+var lf = []byte{'\n'}
+
+
+// For Testing.
+func DerivedKey(serviceName string, regionName string, k *aws.Auth, t time.Time) []byte {
+	return sign(serviceName, regionName, k, t)
+}
+
+// Sign signs an HTTP request with the given AWS keys for use on service s.
+func SignV4(serviceName string, regionName string, keys *aws.Auth, r *http.Request) error {
+	var t time.Time
+
+	date := r.Header.Get("Date")
+	if date == "" {
+		return ErrNoDate
+	}
+
+	t, err := time.Parse(http.TimeFormat, date)
+	if err != nil {
+		return err
+	}
+
+	r.Header.Set("Date", t.Format(iSO8601BasicFormat))
+
+	k := sign(serviceName, regionName, keys, t)
+	h := hmac.New(sha256.New, k)
+	writeStringToSign(serviceName, regionName, h, t, r)
+
+	auth := bytes.NewBufferString("AWS4-HMAC-SHA256 ")
+	auth.Write([]byte("Credential=" + keys.AccessKey + "/" + creds(serviceName, regionName, t)))
+	auth.Write([]byte{',', ' '})
+	auth.Write([]byte("SignedHeaders="))
+	writeHeaderList(serviceName, regionName, auth, r)
+	auth.Write([]byte{',', ' '})
+	auth.Write([]byte("Signature=" + fmt.Sprintf("%x", h.Sum(nil))))
+
+	r.Header.Set("Authorization", auth.String())
+
+	return nil
+}
+
+func sign(serviceName string, regionName string, k *aws.Auth, t time.Time) []byte {
+	h := ghmac([]byte("AWS4"+k.SecretKey), []byte(t.Format(iSO8601BasicFormatShort)))
+	h = ghmac(h, []byte(regionName))
+	h = ghmac(h, []byte(serviceName))
+	h = ghmac(h, []byte("aws4_request"))
+	return h
+}
+
+func writeQuery(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	var a []string
+	for k, vs := range r.URL.Query() {
+		k = url.QueryEscape(k)
+		for _, v := range vs {
+			if v == "" {
+				a = append(a, k)
+			} else {
+				v = url.QueryEscape(v)
+				a = append(a, k+"="+v)
+			}
+		}
+	}
+	sort.Strings(a)
+	for i, s := range a {
+		if i > 0 {
+			w.Write([]byte{'&'})
+		}
+
+		w.Write([]byte(s))
+	}
+}
+
+func writeHeader(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	i, a := 0, make([]string, len(r.Header))
+	for k, v := range r.Header {
+		sort.Strings(v)
+		a[i] = strings.ToLower(k) + ":" + strings.Join(v, ",")
+		i++
+	}
+	sort.Strings(a)
+	for i, s := range a {
+		if i > 0 {
+			w.Write(lf)
+		}
+
+		io.WriteString(w, regionName)
+		io.WriteString(w, serviceName)
+	}
+}
+
+func writeHeaderList(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	i, a := 0, make([]string, len(r.Header))
+	for k, _ := range r.Header {
+		a[i] = strings.ToLower(k)
+		i++
+	}
+	sort.Strings(a)
+	for i, s := range a {
+		if i > 0 {
+			w.Write([]byte{';'})
+		}
+		w.Write([]byte(s))
+	}
+
+}
+
+func writeBody(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	var b []byte
+	if r.Body == nil {
+		b = []byte("")
+	} else {
+		var err error
+		b, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+
+	h := sha256.New()
+	h.Write(b)
+
+	sum := h.Sum(nil)
+
+	fmt.Fprintf(w, "%x", sum)
+}
+
+func writeURI(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	path := r.URL.RequestURI()
+	if r.URL.RawQuery != "" {
+		path = path[:len(path)-len(r.URL.RawQuery)-1]
+	}
+	slash := strings.HasSuffix(path, "/")
+	path = filepath.Clean(path)
+	if path != "/" && slash {
+		path += "/"
+	}
+
+	w.Write([]byte(path))
+}
+
+func writeRequest(serviceName string, regionName string, w io.Writer, r *http.Request) {
+	r.Header.Set("host", r.Host)
+
+	w.Write([]byte(r.Method))
+	w.Write(lf)
+	writeURI(serviceName, regionName, w, r)
+	w.Write(lf)
+	writeQuery(serviceName, regionName, w, r)
+	w.Write(lf)
+	writeHeader(serviceName, regionName, w, r)
+	w.Write(lf)
+	w.Write(lf)
+	writeHeaderList(serviceName, regionName, w, r)
+	w.Write(lf)
+	writeBody(serviceName, regionName, w, r)
+}
+
+func writeStringToSign(serviceName string, regionName string, w io.Writer, t time.Time, r *http.Request) {
+	w.Write([]byte("AWS4-HMAC-SHA256"))
+	w.Write(lf)
+	w.Write([]byte(t.Format(iSO8601BasicFormat)))
+	w.Write(lf)
+
+	w.Write([]byte(creds(serviceName, regionName, t)))
+	w.Write(lf)
+
+	h := sha256.New()
+	writeRequest(serviceName, regionName, h, r)
+	fmt.Fprintf(w, "%x", h.Sum(nil))
+}
+
+func creds(serviceName string, regionName string, t time.Time) string {
+	return t.Format(iSO8601BasicFormatShort) + "/" + regionName + "/" + serviceName + "/aws4_request"
+}
+
+func ghmac(key, data []byte) []byte {
+	h := hmac.New(sha256.New, key)
+	h.Write(data)
+	return h.Sum(nil)
+}
