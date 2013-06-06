1ab9f61206dfb04227fa44a44d7a73e83d09267d
Matthew Moore <matt@crowdmob.com>
2013-06-06 16:38:32 -0700

    tabs to 2 spaces fix

--- dynamodb/attribute.go
+++ dynamodb/attribute.go
@@ -5,9 +5,9 @@ import (
 )
 
 const (
-	TYPE_STRING                         = "S"
-	TYPE_NUMBER                         = "N"
-	TYPE_BINARY                         = "B"
+  TYPE_STRING                         = "S"
+  TYPE_NUMBER                         = "N"
+  TYPE_BINARY                         = "B"
 
   TYPE_STRING_SET                     = "SS"
   TYPE_NUMBER_SET                     = "NS"
@@ -15,30 +15,30 @@ const (
 
   COMPARISON_EQUAL                    = "EQ"
   COMPARISON_NOT_EQUAL                = "NE"
- 	COMPARISON_LESS_THAN_OR_EQUAL       = "LE"
- 	COMPARISON_LESS_THAN                = "LT"
+  COMPARISON_LESS_THAN_OR_EQUAL       = "LE"
+  COMPARISON_LESS_THAN                = "LT"
   COMPARISON_GREATER_THAN_OR_EQUAL    = "GE"
- 	COMPARISON_GREATER_THAN             = "GT"
+  COMPARISON_GREATER_THAN             = "GT"
   COMPARISON_ATTRIBUTE_EXISTS         = "NOT_NULL"
   COMPARISON_ATTRIBUTE_DOES_NOT_EXIST = "NULL"
   COMPARISON_CONTAINS                 = "CONTAINS"
   COMPARISON_DOES_NOT_CONTAIN         = "NOT_CONTAINS"
- 	COMPARISON_BEGINS_WITH              = "BEGINS_WITH"
+  COMPARISON_BEGINS_WITH              = "BEGINS_WITH"
   COMPARISON_IN                       = "IN"
- 	COMPARISON_BETWEEN                  = "BETWEEN"
+  COMPARISON_BETWEEN                  = "BETWEEN"
 )
 
 
 
 type PrimaryKey struct {
-	KeyAttribute        *Attribute
-	RangeAttribute      *Attribute
+  KeyAttribute        *Attribute
+  RangeAttribute      *Attribute
 }
 
 type Attribute struct {
-	Type                string
-	Name                string
-	Value               string
+  Type                string
+  Name                string
+  Value               string
 }
 
 type AttributeComparison struct {
@@ -66,47 +66,47 @@ func NewEqualStringAttributeComparison(attributeName string, equalToValue string
 }
 
 func NewStringAttribute(name string, value string) *Attribute {
-	return &Attribute{TYPE_STRING,
-		name,
-		value,
-	}
+  return &Attribute{TYPE_STRING,
+    name,
+    value,
+  }
 }
 
 func NewNumericAttribute(name string, value string) *Attribute {
-	return &Attribute{TYPE_NUMBER,
-		name,
-		value,
-	}
+  return &Attribute{TYPE_NUMBER,
+    name,
+    value,
+  }
 }
 
 func NewBinaryAttribute(name string, value string) *Attribute {
-	return &Attribute{TYPE_BINARY,
-		name,
-		value,
-	}
+  return &Attribute{TYPE_BINARY,
+    name,
+    value,
+  }
 }
 
 func (k *PrimaryKey) HasRange() bool {
-	return k.RangeAttribute != nil
+  return k.RangeAttribute != nil
 }
 
 // Useful when you may have many goroutines using a primary key, so they don't fuxor up your values.
 func (k *PrimaryKey) Clone(h string, r string) []Attribute {
-	pk := &Attribute{ k.KeyAttribute.Type,
-		k.KeyAttribute.Name,
-		h,
-	}
+  pk := &Attribute{ k.KeyAttribute.Type,
+    k.KeyAttribute.Name,
+    h,
+  }
 
-	result := []Attribute{*pk}
+  result := []Attribute{*pk}
 
-	if k.HasRange() {
-		rk := &Attribute{ k.RangeAttribute.Type,
-			k.RangeAttribute.Name,
-			r,
-		}
+  if k.HasRange() {
+    rk := &Attribute{ k.RangeAttribute.Type,
+      k.RangeAttribute.Name,
+      r,
+    }
 
-		result = append(result, *rk)
-	}
+    result = append(result, *rk)
+  }
 
-	return result
+  return result
 }
