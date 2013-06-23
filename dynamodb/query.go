e8b70f3d94ae50eb832c8c1114d26ec38548ff03
Lucas Waller <lucas@waller.me>
2013-06-23 14:36:20 +0200

    fixed local secondary index queries

---
+++ dynamodb/query.go
@@ -0,0 +1,37 @@
+package dynamodb
+
+import (
+	"errors"
+	"fmt"
+  simplejson "github.com/bitly/go-simplejson"
+)
+
+func (t *Table) Query(indexName string, attributeComparisons []AttributeComparison) ([]map[string]*Attribute, error) {
+	q := NewQuery(t)
+  q.ConsistentRead(true)
+  q.AddIndexName(indexName)
+  q.AddKeyConditions(attributeComparisons)
+	jsonResponse, err := t.Server.queryServer(target("Query"), q)
+	if err != nil { return nil, err	}
+
+	json, err := simplejson.NewJson(jsonResponse)
+	if err != nil { return nil, err	}
+
+  itemCount, err := json.Get("Count").Int()
+	if err != nil {
+		message := fmt.Sprintf("Unexpected response %s", jsonResponse)
+		return nil, errors.New(message)
+	}
+
+  results := make([]map[string]*Attribute, itemCount)
+
+  for i, _ := range results {
+  	item, err := json.Get("Items").GetIndex(i).Map()
+  	if err != nil {
+  		message := fmt.Sprintf("Unexpected response %s", jsonResponse)
+  		return nil, errors.New(message)
+  	}
+    results[i] = parseAttributes(item)
+  }
+	return results, nil
+}
