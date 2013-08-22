2c7137a6ac108d66a572dc94c4958ca33b9ce6f0
Ali Moeeny <alimoeeny@gmail.com>
2013-08-22 10:02:45 -0400

    If aws.Auth.Token is not empty add it to the X-Amz-Security-Token reqyest header
    If aws.Auth.Token is not empty add it to the X-Amz-Security-Token reqyest header

--- s3/s3.go
+++ s3/s3.go
@@ -529,6 +529,9 @@ func (s3 *S3) prepare(req *request) error {
 	}
 	req.headers["Host"] = []string{u.Host}
 	req.headers["Date"] = []string{time.Now().In(time.UTC).Format(time.RFC1123)}
+	if s3.Auth.SecurityToken != "" {
+        	req.headers["X-Amz-Security-Token"] = s3.Auth.SecurityToken
+        }
 	sign(s3.Auth, req.method, req.signpath, req.params, req.headers)
 	return nil
 }
