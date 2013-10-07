4dfbd101f0e2731abb5de392a6fe77b1c1b66dd9
John Berthels <john@berthels.co.uk>
2013-10-07 11:52:38 +0100

    cherry picked  005bee7796eba1354e25995289bbd1313db6fbcc

--- dynamodb/dynamodb.go
+++ dynamodb/dynamodb.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"log"
+	"errors"
 )
 
 type Server struct {
@@ -33,6 +35,47 @@ func NewQuery(queryParts []string) *Query {
 // func (s *Server) QueryServer(target string, query *Query) ([]byte, error) {
 // 	return s.queryServer(target, query)
 // }
+// Specific error constants
+var ErrNotFound = errors.New("Item not found")
+
+// Error represents an error in an operation with Dynamodb (following goamz/s3)
+type Error struct {
+	StatusCode	int    // HTTP status code (200, 403, ...)
+	Status		string
+	Code		string // Dynamodb error code ("MalformedQueryString", ...)
+	Message		string // The human-oriented error message
+}
+
+func (e *Error) Error() string {
+	return e.Code + ": " + e.Message
+}
+
+func buildError(r *http.Response, jsonBody []byte) error {
+
+	ddbError := Error{
+		StatusCode:	r.StatusCode,
+		Status:		r.Status,
+	}
+	// TODO return error if Unmarshal fails?
+
+	json, err := simplejson.NewJson(jsonBody)
+	if err != nil {
+		log.Printf("Failed to parse body as JSON")
+		return err
+	}
+	ddbError.Message = json.Get("message").MustString()
+
+	// Of the form: com.amazon.coral.validate#ValidationException
+	// We only want the last part
+	codeStr := json.Get("__type").MustString()
+	hashIndex := strings.Index(codeStr, "#")
+	if hashIndex > 0 {
+		codeStr = codeStr[hashIndex+1:]
+	}
+	ddbError.Code = codeStr
+
+	return &ddbError
+}
 
 func (s *Server) queryServer(target string, query *Query) ([]byte, error) {
 	data := strings.NewReader(query.String())
