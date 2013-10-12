73892f7463abc4442eed6d4c39c7ec0462db66a1
Sarah Adams <sadams.codes@gmail.com>
2013-10-12 14:46:26 -0700

    revert import to remote

--- dynamodb/query_builder_test.go
+++ dynamodb/query_builder_test.go
@@ -3,7 +3,7 @@ package dynamodb_test
 import (
 	simplejson "github.com/bitly/go-simplejson"
 	"github.com/crowdmob/goamz/aws"
-	"../dynamodb"
+	"github.com/crowdmob/goamz/dynamodb"
 	"testing"
 )
 
