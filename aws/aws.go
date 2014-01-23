ccb5b7fbfeea6f2511ea7323d61187a93a599896
Billy Bastardi <billy@yext.com>
2014-01-23 18:36:45 -0500

    add autoscaling back to struct

--- aws/aws.go
+++ aws/aws.go
@@ -53,6 +53,7 @@ type Region struct {
 	ELBEndpoint            string
 	DynamoDBEndpoint       string
 	CloudWatchServicepoint ServiceInfo
+	AutoScalingEndpoint    string
 }
 
 var Regions = map[string]Region{
