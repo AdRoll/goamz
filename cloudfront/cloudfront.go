cfd6e53580db9b1de673431876d0affed272756a
Chris Bannister <chris.bannister@swiftkey.net>
2014-03-28 14:52:45 +0000

    Add support for signed canned cloudfront urls

--- cloudfront/cloudfront.go
+++ cloudfront/cloudfront.go
@@ -1,34 +1,141 @@
 package cloudfront
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/sha1"
 	"encoding/base64"
-	"github.com/crowdmob/goamz/aws"
-	"log"
+	"encoding/json"
+	"fmt"
+	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
 type CloudFront struct {
-	auth    aws.Auth
-	BaseURL string
+	BaseURL   string
+	keyPairId string
+	key       *rsa.PrivateKey
 }
 
-func New(auth aws.Auth, baseurl string) *CloudFront {
-	return &CloudFront{auth: auth, BaseURL: baseurl}
+var base64Replacer = strings.NewReplacer("=", "_", "+", "-", "/", "~")
+
+func New(baseurl string, key *rsa.PrivateKey, keyPairId string) *CloudFront {
+	return &CloudFront{
+		BaseURL:   baseurl,
+		keyPairId: keyPairId,
+		key:       key,
+	}
+}
+
+type epochTime struct {
+	EpochTime int64 `json:"AWS:EpochTIme"`
+}
+
+type condition struct {
+	DateLessThan epochTime
+}
+
+type statement struct {
+	Resource  string
+	Condition condition
+}
+
+type policy struct {
+	Statement []statement
+}
+
+func buildPolicy(resource string, expireTime time.Time) ([]byte, error) {
+	p := &policy{
+		Statement: []statement{
+			statement{
+				Resource: resource,
+				Condition: condition{
+					DateLessThan: epochTime{
+						EpochTime: expireTime.Truncate(time.Millisecond).Unix(),
+					},
+				},
+			},
+		},
+	}
+
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+	if err := enc.Encode(p); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (cf *CloudFront) generateSignature(policy []byte) (string, error) {
+	hash := sha1.New()
+	if _, err := hash.Write(policy); err != nil {
+		return "", err
+	}
+
+	hashed := hash.Sum(nil)
+
+	signed, err := rsa.SignPKCS1v15(rand.Reader, cf.key, crypto.SHA1, hashed)
+	if err != nil {
+		return "", err
+	}
+
+	encoded := base64Replacer.Replace(base64.StdEncoding.EncodeToString(signed))
+
+	return encoded, nil
+}
+
+// Creates a signed url using RSAwithSHA1 as specified by
+// http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-creating-signed-url-canned-policy.html#private-content-canned-policy-creating-signature
+func (cf *CloudFront) CannedSignedURL(path string, query url.Values, expires time.Time) (string, error) {
+	resource := path
+	if query == nil {
+		query = make(url.Values)
+	} else {
+		resource += "?" + query.Encode()
+	}
+
+	policy, err := buildPolicy(resource, expires)
+	if err != nil {
+		return "", err
+	}
+
+	signature, err := cf.generateSignature(policy)
+	if err != nil {
+		return "", err
+	}
+
+	// TOOD: Do this once
+	uri, err := url.Parse(cf.BaseURL)
+	if err != nil {
+		return "", err
+	}
+
+	query.Set("Expires", fmt.Sprintf("%d", (expires.Truncate(time.Millisecond).Unix())))
+	query.Set("Signature", signature)
+	query.Set("Key-Pair-Id", cf.keyPairId)
+
+	uri.Path = path
+	uri.RawQuery = query.Encode()
+
+	return uri.String(), nil
 }
 
 func (cloudfront *CloudFront) SignedURL(path, querystrings string, expires time.Time) string {
-	policy := `{"Statement":[{"Resource":"` + cloudfront.BaseURL + "?" + querystrings + `,"Condition":{"DateLessThan":{"AWS:EpochTime":` + strconv.FormatInt(expires.Unix(), 10) + `}}}]}`
+	policy := `{"Statement":[{"Resource":"` + path + "?" + querystrings + `,"Condition":{"DateLessThan":{"AWS:EpochTime":` + strconv.FormatInt(expires.Truncate(time.Millisecond).Unix(), 10) + `}}}]}`
 
-	log.Printf("Policy: %v\n", policy)
 	hash := sha1.New()
-	b := hash.Sum([]byte(policy))
+	hash.Write([]byte(policy))
+	b := hash.Sum(nil)
 	he := base64.StdEncoding.EncodeToString(b)
 
 	policySha1 := he
-	keypairid_str := cloudfront.auth.AccessKey
 
-	url := cloudfront.BaseURL + path + "?" + querystrings + "&Expires=" + strconv.FormatInt(expires.Unix(), 10) + "&Signature=" + policySha1 + "&Key-Pair-Id=" + keypairid_str
+	url := cloudfront.BaseURL + path + "?" + querystrings + "&Expires=" + strconv.FormatInt(expires.Unix(), 10) + "&Signature=" + policySha1 + "&Key-Pair-Id=" + cloudfront.keyPairId
+
 	return url
 }
