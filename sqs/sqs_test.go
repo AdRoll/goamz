9940dea233516565af0c557006b04f6ee29bc3a5
Ali Moeeny <alimoeeny@gmail.com>
2014-09-05 09:21:14 -0400

    changeing the test not to be sensitive to the order of attributes

--- sqs/sqs_test.go
+++ sqs/sqs_test.go
@@ -60,10 +60,26 @@ func (s *S) TestCreateQueueWithAttributes(c *check.C) {
 	req := testServer.WaitRequest()
 
 	// TestCreateQueue() tests the core functionality, just check the timeout in this test
-	c.Assert(req.Form["Attribute.1.Name"], check.DeepEquals, []string{"ReceiveMessageWaitTimeSeconds"})
-	c.Assert(req.Form["Attribute.1.Value"], check.DeepEquals, []string{"20"})
-	c.Assert(req.Form["Attribute.2.Name"], check.DeepEquals, []string{"MessageRetentionPeriod"})
-	c.Assert(req.Form["Attribute.2.Value"], check.DeepEquals, []string{"60"})
+
+	// Since attributes is a map the order is random,
+	// So I modified the test so that it will not be sensitive to the order of the two attributes,
+	// And I don't know how to write assertions like this with gocheck so I write it in plain go test stuff
+	if !(reflect.DeepEqual(req.Form["Attribute.1.Name"], []string{"ReceiveMessageWaitTimeSeconds"}) ||
+		reflect.DeepEqual(req.Form["Attribute.2.Name"], []string{"ReceiveMessageWaitTimeSeconds"})) {
+		t.Fail()
+	}
+	if !(reflect.DeepEqual(req.Form["Attribute.1.Value"], []string{"20"}) ||
+		reflect.DeepEqual(req.Form["Attribute.2.Value"], []string{"20"})) {
+		t.Fail()
+	}
+	if !(reflect.DeepEqual(req.Form["Attribute.1.Name"], []string{"MessageRetentionPeriod"}) ||
+		reflect.DeepEqual(req.Form["Attribute.2.Name"], []string{"MessageRetentionPeriod"})) {
+		t.Fail()
+	}
+	if !(reflect.DeepEqual(req.Form["Attribute.1.Value"], []string{"60"}) ||
+		reflect.DeepEqual(req.Form["Attribute.2.Value"], []string{"60"})) {
+		t.Fail()
+	}
 }
 
 func (s *S) TestListQueues(c *check.C) {
