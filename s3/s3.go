608934ec7e58cfe206dd824c684b0e71693e2635
Dylan Meissner <dylanmei@gmail.com>
2014-09-13 18:27:43 -0700

    Remove extra s3 hostname

--- s3/s3.go
+++ s3/s3.go
@@ -903,7 +903,6 @@ func (s3 *S3) setupHttpRequest(req *request) (*http.Request, error) {
 
 	hreq := http.Request{
 		URL:        u,
-		Host:       "s3.amazonaws.com",
 		Method:     req.method,
 		ProtoMajor: 1,
 		ProtoMinor: 1,
