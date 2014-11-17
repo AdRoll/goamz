57cf92da0c676b2eed88a43afca063694fd69a23
Chris Broglie <cbroglie@zynga.com>
2014-11-16 23:26:46 -0800

    Implement ToDynamo/FromDynamo functions
    These functions convert in memory objects to and from DyanomDB
    attribute value objects.
    
    Reference: http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_AttributeValue.html

---
+++ dynamodb/dynamizer/dynamizer.go
@@ -0,0 +1,227 @@
+package dynamizer
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
+	"strconv"
+)
+
+// A DynamoAttribute represents the union of possible DynamoDB attribute values.
+// See http://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_AttributeValue.html
+//
+// Since the intention is to use this with JSON documents (or structs
+// representing JSON documents), the binary and set types have been omitted.
+type DynamoAttribute struct {
+	S    *string                     `json:",omitempty"` // a pointer so we can represent the empty string
+	N    string                      `json:",omitempty"`
+	BOOL string                      `json:",omitempty"`
+	NULL string                      `json:",omitempty"`
+	M    map[string]*DynamoAttribute `json:",omitempty"`
+	L    []*DynamoAttribute          `json:",omitempty"`
+}
+
+// A DynamoItem represents a the top level item stored in DyanmoDB.
+type DynamoItem map[string]*DynamoAttribute
+
+// ToDynamo accepts a map or struct in basic JSON format and converts it to a
+// map which can be JSON-encoded into the DynamoDB format.
+//
+// If in is a struct, we first JSON encode/decode it to get the data as a map.
+// This can/should be optimized later.
+func ToDynamo(in interface{}) (item DynamoItem, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(runtime.Error); ok {
+				err = e
+			} else if s, ok := r.(string); ok {
+				err = errors.New(s)
+			} else {
+				err = r.(error)
+			}
+			item = nil
+		}
+	}()
+
+	v := reflect.ValueOf(in)
+	switch v.Kind() {
+	case reflect.Struct:
+		item = dynamizeStruct(in)
+	case reflect.Map:
+		item = dynamizeMap(in)
+	default:
+		return nil, errors.New("Top level object must be a struct or map")
+	}
+
+	return item, nil
+}
+
+// FromDynamo takes a map of DynamoDB attributes and converts it into a map or
+// struct in basic JSON format.
+//
+// If v points to a struct, we first convert it to a basic map, then JSON
+// encode/decode it to convert to a struct. This can/should be optimized later.
+func FromDynamo(item DynamoItem, v interface{}) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(runtime.Error); ok {
+				err = e
+			} else if s, ok := r.(string); ok {
+				err = errors.New(s)
+			} else {
+				err = r.(error)
+			}
+			item = nil
+		}
+	}()
+
+	m := make(map[string]interface{})
+	for k, v := range item {
+		m[k] = undynamize(v)
+	}
+
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return errors.New("v must be a non-nil pointer to a map[string]interface{} or struct")
+	}
+
+	switch rv.Elem().Kind() {
+	case reflect.Struct:
+		// TODO: We convert basic maps into structs by JSON encoding/decoding.
+		// This can be made more efficient by simplying recursing over the
+		// struct and populating it from the map.
+		b, err := json.Marshal(m)
+		if err != nil {
+			return err
+		}
+		return json.Unmarshal(b, v)
+	case reflect.Map:
+		if rv.Elem().Type().Key().Kind() != reflect.String {
+			return errors.New("v must be a non-nil pointer to a map[string]interface{} or struct")
+		}
+		rv.Elem().Set(reflect.ValueOf(m))
+	default:
+		return errors.New("v must be a non-nil pointer to a map[string]interface{} or struct")
+	}
+
+	return nil
+}
+
+func dynamizeStruct(in interface{}) DynamoItem {
+	// TODO: We convert structs into basic maps by JSON encoding/decoding. This
+	// can be made more efficient by recursing over the struct directly.
+	b, err := json.Marshal(in)
+	if err != nil {
+		panic(err)
+	}
+	var m map[string]interface{}
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		panic(err)
+	}
+	return dynamizeMap(m)
+}
+
+func dynamizeMap(in interface{}) DynamoItem {
+	item := make(DynamoItem)
+	m := in.(map[string]interface{})
+	for k, v := range m {
+		item[k] = dynamize(v)
+	}
+	return item
+}
+
+func dynamize(in interface{}) *DynamoAttribute {
+	a := &DynamoAttribute{}
+
+	if in == nil {
+		a.NULL = "true"
+		return a
+	}
+
+	if m, ok := in.(map[string]interface{}); ok {
+		a.M = make(map[string]*DynamoAttribute)
+		for k, v := range m {
+			a.M[k] = dynamize(v)
+		}
+		return a
+	}
+
+	if l, ok := in.([]interface{}); ok {
+		a.L = make([]*DynamoAttribute, len(l))
+		for index, v := range l {
+			a.L[index] = dynamize(v)
+		}
+		return a
+	}
+
+	// Only primitive types should remain.
+	v := reflect.ValueOf(in)
+	switch v.Kind() {
+	case reflect.Bool:
+		if v.Bool() {
+			a.BOOL = "true"
+		} else {
+			a.BOOL = "false"
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		a.N = strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		a.N = strconv.FormatUint(v.Uint(), 10)
+	case reflect.Float32, reflect.Float64:
+		a.N = strconv.FormatFloat(v.Float(), 'f', -1, 64)
+	case reflect.String:
+		a.S = new(string)
+		*a.S = v.String()
+	default:
+		panic(fmt.Sprintf(`the type %s is not supported`, v.Kind()))
+	}
+
+	return a
+}
+
+func undynamize(a *DynamoAttribute) interface{} {
+	if a.S != nil {
+		return *a.S
+	}
+
+	if a.N != "" {
+		n, err := strconv.ParseFloat(a.N, 64)
+		if err != nil {
+			panic(err)
+		}
+		return n
+	}
+
+	if a.BOOL != "" {
+		if a.BOOL == "true" {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	if a.NULL != "" {
+		return nil
+	}
+
+	if a.M != nil {
+		m := make(map[string]interface{})
+		for k, v := range a.M {
+			m[k] = undynamize(v)
+		}
+		return m
+	}
+
+	if a.L != nil {
+		l := make([]interface{}, len(a.L))
+		for index, v := range a.L {
+			l[index] = undynamize(v)
+		}
+		return l
+	}
+
+	panic(fmt.Sprintf("unsupported dynamo attribute %+v", a))
+}
