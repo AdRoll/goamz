f7a5bce0fbde72c31f35712f09d20b8b24ceae33
Ian Kent <email@iankent.co.uk>
2014-12-11 15:44:14 +0000

    Add support for describing cache replication groups

--- elasticache/elasticache.go
+++ elasticache/elasticache.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 
@@ -20,10 +21,69 @@ type ElastiCache struct {
 	aws.Region
 }
 
+// DescribeReplicationGroupsResult represents the response
+type DescribeReplicationGroupsResult struct {
+	ReplicationGroups []ReplicationGroup `xml:"DescribeReplicationGroupsResult>ReplicationGroups"`
+}
+
+func (repGroup *ReplicationGroup) GetPrimaryNode() (*PrimaryEndpoint, error) {
+	if repGroup.Status != "available" {
+		return nil, errors.New("Replication group isn't available")
+	}
+
+	if len(repGroup.NodeGroups) == 0 {
+		return nil, errors.New("No node groups available")
+	}
+
+	ng := repGroup.NodeGroups[0]
+
+	if &ng.PrimaryEndpoint == nil {
+		return nil, errors.New("No primary node found")
+	}
+
+	return &ng.PrimaryEndpoint, nil
+}
+
 // DescribeCacheClustersResult represents the response from a
 // DescribeCacheClusters ElastiCache API call
 type DescribeCacheClustersResult struct {
 	CacheClusters []*CacheCluster `xml:"DescribeCacheClustersResult>CacheClusters"`
+}
+
+// ReplicationGroup represents a replication group
+type ReplicationGroup struct {
+	Status             string      `xml:"ReplicationGroup>Status"`
+	ReplicationGroupId string      `xml:"ReplicationGroup>ReplicationGroupId"`
+	MemberClusters     []string    `xml:"ReplicationGroup>MemberClusters>ClusterId"`
+	NodeGroups         []NodeGroup `xml:"ReplicationGroup>NodeGroups"`
+}
+
+// NodeGroup represents a node group
+type NodeGroup struct {
+	Status           string             `xml:"NodeGroup>Status"`
+	PrimaryEndpoint  PrimaryEndpoint    `xml:"NodeGroup>PrimaryEndpoint"`
+	NodeGroupMembers []*NodeGroupMember `xml:"NodeGroup>NodeGroupMembers>NodeGroupMember"`
+}
+
+// NodeGroupMember represents an individual node
+type NodeGroupMember struct {
+	CurrentRole               string       `xml:"CurrentRole"`
+	PreferredAvailabilityZone string       `xml:"PreferredAvailabilityZone"`
+	CacheNodeId               string       `xml:"CacheNodeId"`
+	CacheClusterId            string       `xml:"CacheClusterId"`
+	ReadEndpoint              ReadEndpoint `xml:"ReadEndpoint"`
+}
+
+// PrimaryEndpoint represents the primary endpoint
+type PrimaryEndpoint struct {
+	Port    int    `xml:"Port"`
+	Address string `xml:"Address"`
+}
+
+// ReadEndpoint represents a read endpoint
+type ReadEndpoint struct {
+	Port    int    `xml:"Port"`
+	Address string `xml:"Address"`
 }
 
 // CacheCluster represents a cache cluster
@@ -48,10 +108,26 @@ func New(auth aws.Auth, region aws.Region) *ElastiCache {
 	return &ElastiCache{auth, region}
 }
 
-// Describe returns information about a cache cluster
-func (ec *ElastiCache) Describe(cluster string) (*CacheCluster, error) {
+// DescribeReplicationGroup returns information about a cache replication group
+func (ec *ElastiCache) DescribeReplicationGroup(groupName string) (*ReplicationGroup, error) {
+	var resp DescribeReplicationGroupsResult
+	err := ec.query("Action=DescribeReplicationGroups&ReplicationGroupId="+groupName+"&Version=2014-07-15", &resp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.ReplicationGroups) == 0 {
+		return nil, errors.New("Replication group not found")
+	}
+
+	return &resp.ReplicationGroups[0], nil
+}
+
+// DescribeCacheCluster returns information about a cache cluster
+func (ec *ElastiCache) DescribeCacheCluster(cluster string) (*CacheCluster, error) {
 	var resp DescribeCacheClustersResult
-	err := ec.query("Action=DescribeCacheClusters&CacheClusterId="+cluster+"&ShowCacheNodeInfo=true", &resp)
+	err := ec.query("Action=DescribeCacheClusters&CacheClusterId="+cluster+"&ShowCacheNodeInfo=true&Version=2014-07-15", &resp)
 
 	if err != nil {
 		return nil, err
@@ -100,6 +176,8 @@ func (ec *ElastiCache) query(query string, response interface{}) error {
 		return err
 	}
 
+	log.Println(hreq)
+	log.Println(string(b))
 	return xml.Unmarshal(b, &response)
 }
 
